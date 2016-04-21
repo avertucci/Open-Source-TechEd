9f56bc786ed6ad789f8ccf086bf885f88a3c9e7f
avertucci <vertucci13@gmail.com>
2016-04-20 20:05:21 -0400

    Updated contact form PHP

--- php/contact.php
+++ php/contact.php
@@ -7,11 +7,11 @@
 
 echo $name AND $email AND $phone AND $address AND $city AND $cuntery AND $code;
 
-$to = 'ahmederaqi5@gmail.com';
+$to = 'vertucci13@gmail.com';
 $subject = 'the subject';
 $message = "name = : $name <br/> email is : $email <br/>phone: $phone <br/> address:  $address <br/> city : $city <br/>country : $cuntery <br/> code: $code  ";
-$headers = 'From: webmaster@example.com' . "\r\n" .
-        'Reply-To: webmaster@example.com' . "\r\n" .
+$headers = 'From: Open Source Technology Education' . "\r\n" .
+        'Reply-To: vertucci13@gmail.com' . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
